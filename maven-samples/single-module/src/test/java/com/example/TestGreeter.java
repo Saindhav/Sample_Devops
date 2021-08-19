fbd263ef2e0cf6441c595b31abc5abcf9ac89298
saindhav <sain.nihal@gmail.com>
2021-08-19 13:28:05 +0530

    Added new test method greetShouldNotIncludeGreetingMessage

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,4 +40,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+  @Test
+  public void greetShouldNotIncludeGreetingMessage() {
+    String someone = "World, Thanks you making me Happy";
+
+    assertThat(greeter.greet(someone).length(), is(lessThan(someone.length())));
+  }
 }
